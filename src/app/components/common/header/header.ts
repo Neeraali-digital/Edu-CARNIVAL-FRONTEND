605cd288f8a9fa9b29f6b0aa73be28a23bd853fa
Neeraali-digital <neeraalidigitalweb@gmail.com>
2026-02-16 18:27:47 +0530

    content a and design changes

--- src/app/components/common/header/header.ts
+++ src/app/components/common/header/header.ts
@@ -11,12 +11,21 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   isMobileMenuOpen = false;
+  activeMobileSection: string | null = null;
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    if (!this.isMobileMenuOpen) {
+      this.activeMobileSection = null;
+    }
   }
 
   closeMobileMenu() {
     this.isMobileMenuOpen = false;
+    this.activeMobileSection = null;
+  }
+
+  toggleMobileSection(section: string) {
+    this.activeMobileSection = this.activeMobileSection === section ? null : section;
   }
 }
